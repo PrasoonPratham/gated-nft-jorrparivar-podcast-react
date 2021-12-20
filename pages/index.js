fc6cf9a72762332a3fc353633fd215045af7d8a3
Pratham Prasoon <42303128+PrasoonPratham@users.noreply.github.com>
2021-12-20 22:24:05 +0530

    cleanup

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,9 @@ import axios from "axios";
 import { ConnectWallet } from "@3rdweb/react";
 import { useWeb3 } from "@3rdweb/hooks";
 import Head from "next/head";
-
+import { useMoralis } from "react-moralis";
 import "regenerator-runtime/runtime";
+import Link from "next/link";
 
 
 
@@ -44,78 +45,41 @@ export default function App() {
             </p>
           </div>
 
-          <button className="w-full mt-5">
-            <ConnectWallet>Connect Metamask</ConnectWallet>
-          </button>
+          
 
           {address ? (
-                      <form className="mt-3 space-y-3" action="#" method="POST">
+                      // <form className="mt-3 space-y-3" action="#" method="POST">
             <div>
               <div class="flex flex-col items-center justify-center  px-4">
                 <div class="flex flex-col space-y-6">
-                  <div class="text-center font-bold">Click Below To Watch Podcast!</div>
-                  <div class="flex flex-row items-center bg-white border px-4 py-2 rounded-3xl shadow-md">
-                    <button class="flex items-center justify-center bg-indigo-600 hover:bg-indigo-800 rounded-full h-8 w-10">
-                      <svg
-                        class="w-6 h-6 text-white"
-                        fill="none"
-                        stroke="currentColor"
-                        viewBox="0 0 24 24"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="1.5"
-                          d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z"
-                        ></path>
-                        <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="1.5"
-                          d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                        ></path>
-                      </svg>
-                    </button>
-                    <div class="flex flex-row items-center space-x-px ml-4">
-                      <div class="h-2 w-1 bg-gray-500 rounded-lg"></div>
-                      <div class="h-2 w-1 bg-gray-500 rounded-lg"></div>
-                      <div class="h-4 w-1 bg-gray-500 rounded-lg"></div>
-                      <div class="h-8 w-1 bg-gray-500 rounded-lg"></div>
-                      <div class="h-8 w-1 bg-gray-500 rounded-lg"></div>
-                      <div class="h-10 w-1 bg-gray-500 rounded-lg"></div>
-                      <div class="h-10 w-1 bg-gray-500 rounded-lg"></div>
-                      <div class="h-12 w-1 bg-gray-500 rounded-lg"></div>
-                      <div class="h-10 w-1 bg-gray-500 rounded-lg"></div>
-                      <div class="h-6 w-1 bg-gray-500 rounded-lg"></div>
-                      <div class="h-5 w-1 bg-gray-500 rounded-lg"></div>
-                      <div class="h-4 w-1 bg-gray-500 rounded-lg"></div>
-                      <div class="h-3 w-1 bg-gray-500 rounded-lg"></div>
-                      <div class="h-2 w-1 bg-gray-500 rounded-lg"></div>
-                      <div class="h-2 w-1 bg-gray-500 rounded-lg"></div>
-                      <div class="h-2 w-1 bg-gray-500 rounded-lg"></div>
-                      <div class="h-10 w-1 bg-gray-500 rounded-lg"></div>
-                      <div class="h-2 w-1 bg-gray-500 rounded-lg"></div>
-                      <div class="h-10 w-1 bg-gray-500 rounded-lg"></div>
-                      <div class="h-8 w-1 bg-gray-500 rounded-lg"></div>
-                      <div class="h-8 w-1 bg-gray-500 rounded-lg"></div>
-                      <div class="h-1 w-1 bg-gray-500 rounded-lg"></div>
-                      <div class="h-1 w-1 bg-gray-500 rounded-lg"></div>
-                      <div class="h-2 w-1 bg-gray-500 rounded-lg"></div>
-                      <div class="h-8 w-1 bg-gray-500 rounded-lg"></div>
-                      <div class="h-8 w-1 bg-gray-500 rounded-lg"></div>
-                      <div class="h-2 w-1 bg-gray-500 rounded-lg"></div>
-                      <div class="h-2 w-1 bg-gray-500 rounded-lg"></div>
-                      <div class="h-2 w-1 bg-gray-500 rounded-lg"></div>
-                      <div class="h-2 w-1 bg-gray-500 rounded-lg"></div>
-                      <div class="h-4 w-1 bg-gray-500 rounded-lg"></div>
-                    </div>
-                  </div>
+                  <span class="text-center py-3 px-6 text-white rounded bg-green-400 shadow-lg block md:inline-block font-medium">Wallet Connected! Click below to watch the podcast</span>
+                                             <button
+                                type="submit"
+                                className="flex justify-center w-full px-4 py-3 font-semibold text-white transition-transform transform bg-blue-600 rounded-md shadow-lg outline-none focus:ring-4 active:scale-x-75"
+                            >
+                                <svg
+                                    className="w-6 h-6"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke="currentColor"
+                                >
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth={2}
+                                        d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4"
+                                    />
+                                </svg>
+                      
+                      <Link href="/video"><span className="ml-2">Play Podcast</span></Link>
+                            </button>
                 </div>
               </div>
             </div>
-          </form>
-          ) : null}
+          // </form>
+          ) : <button className="w-full mt-5">
+            <ConnectWallet>Connect Metamask</ConnectWallet>
+          </button>}
 
 
         </div>
